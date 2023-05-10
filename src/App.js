7ee1a3467fc171c8f12039f57561ea9f8620bd61
Jonszr <s13712651652@gmail.com>
2023-05-10 16:11:36 -0600

    fix the page scroll position restore problem

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 
 
-import { useRoutes } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation, useRoutes } from "react-router-dom";
 import routes from "./routes/routes";
-
 const App = () => {
+  const location = useLocation();
+  useEffect(()=>{
+    window.scrollTo(0,0);
+  },[location.pathname])
   const UsemainRoutes = () => {
     const mainRoutes = useRoutes(routes);
+   
     return mainRoutes;
   };
+
   return (
     
     <UsemainRoutes />
