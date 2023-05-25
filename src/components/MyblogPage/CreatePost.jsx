086ffe738257afc5353631a349a4ea4f5aca0fc5
Jonszr <s13712651652@gmail.com>
2023-05-25 13:46:17 -0600

    remove signin with google and github icon

--- src/components/MyblogPage/CreatePost.jsx
+++ src/components/MyblogPage/CreatePost.jsx
@@ -58,7 +58,7 @@ export const CreatePost = () => {
               Cover Image
             </label>
             <input
-              type={'file'}
+              type={'image'}
              
               
               
